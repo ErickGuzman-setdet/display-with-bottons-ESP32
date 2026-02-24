9ee2586bad91772ec01c341269d94e4847acf539
Erick Guzman <69767069+ErickGuzman-setdet@users.noreply.github.com>
2026-02-24 11:25:11 -0600

    Update ESP32_display_botones.py

--- ESP32_display_botones.py
+++ ESP32_display_botones.py
@@ -1,7 +1,7 @@
 from machine import Pin
 import time
 
-# ---- Pines de segmentos ----
+# ---- Establecemos los segmentos del Display ABC---
 A = Pin(2, Pin.OUT)
 B = Pin(4, Pin.OUT)
 C = Pin(5, Pin.OUT)
@@ -10,6 +10,7 @@ E = Pin(19, Pin.OUT)
 F = Pin(13, Pin.OUT)
 G = Pin(12, Pin.OUT)
 
+# ---- Los guardamos en un arreglo -------
 segmentos = [A,B,C,D,E,F,G]
 
 # ---- Tabla que me diste ----
@@ -32,25 +33,27 @@ numeros = [
     [1,0,0,0,1,1,1]  #F
 ]
 
-# ---- Botones ----
+# ---- Pines de Botones en ESP32 ----
 boton_up   = Pin(14, Pin.IN, Pin.PULL_UP)
 boton_down = Pin(27, Pin.IN, Pin.PULL_UP)
 
 numero = 0
 
 def mostrar(n):
-    for i in range(7):
-        segmentos[i].value(numeros[n][i])
+    for i in range(7): # 7 segmentos 
+        segmentos[i].value(numeros[n][i]) #selecciona y asigna el Pin y el valor 0 o 1 para encender/apagar
 
 mostrar(numero)
 
+#Como son 16 segmentos diferentes solamente, cuando llegue al final del arreglo de segmentos el residuo sea 0
+# Esto hace un ciclo de 0 hasta 16, y de 16 a 0 y asi 
 while True:
     if boton_up.value() == 0:
         numero = (numero + 1) % 16
-        mostrar(numero)
-        time.sleep(0.3)
-
+        mostrar(numero)   #Muestra 
+        time.sleep(0.30)
+    
     if boton_down.value() == 0:
         numero = (numero - 1) % 16
         mostrar(numero)
-        time.sleep(0.3)
+        time.sleep(0.30)
